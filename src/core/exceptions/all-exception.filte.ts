26b0b5ed5253679b753776e83e13a7271584edfc
suemor <3100825062@qq.com>
2022-04-16 23:32:23 +0800

    fix: exception logs

--- src/core/exceptions/all-exception.filte.ts
+++ src/core/exceptions/all-exception.filte.ts
@@ -11,7 +11,6 @@ export class AllExceptionFilter<T> implements ExceptionFilter {
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    console.log(exception)
     // @ts-ignore
     const message = exception.response.message
       // @ts-ignore
